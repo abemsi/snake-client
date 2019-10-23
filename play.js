dd8f4fee36d44049438b42b9da01d2da4e1682ca
Amy Bemister <amy.bemister@gmail.com>
2019-10-23 19:26:17 +0000

    Refactored setupInput function

--- play.js
+++ play.js
@@ -1,24 +1,9 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
+setupInput();
 
-const handleUserInput = key => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-}
 
